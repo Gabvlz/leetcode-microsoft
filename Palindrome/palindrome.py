44a5381c32bf87cf084575b1e78dc116394c695e
Gabvlz <gi.valenzuelaleon@gmail.com>
2023-07-25 11:20:43 -0400

    Palindrome with .py, negative number false

---
+++ Palindrome/palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(x):
+    if x < 0:
+        return False
+    reversed_num = 0
+    original_num = x
+    while x > 0:
+        reversed_num = reversed_num * 10 + x % 10
+        x //= 10
+    return original_num == reversed_num
