77f724e2256995f570cf57bf6168417e9aed8cef
Gabvlz <gi.valenzuelaleon@gmail.com>
2023-07-26 15:52:14 -0400

    Replaced ' _ ' for ' - ' errortip

--- Check Palindrome String/isPalindromewithString.ts
+++ Check Palindrome String/isPalindromewithString.ts
@@ -1,5 +1,5 @@
 function isPalindrome(s: string): boolean {
-    s = s.toLowerCase().replace(/[^a-zA-Z0_9]/g,'');//Primero se indican todas las letras a minusculas y se reemplaza todos los caracteres que nosean alfanumericos por una cadena vacía 
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g,'');//Primero se indican todas las letras a minusculas y se reemplaza todos los caracteres que nosean alfanumericos por una cadena vacía 
     let left = 0;
     let right = s.length - 1;//Se da inicio a las variables que representan los indices primero y último
     while(left<right){//Si el indice primero es menor que el último se cumple la condición y se da inicio al bucle
