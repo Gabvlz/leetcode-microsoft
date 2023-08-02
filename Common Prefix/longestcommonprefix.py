5fe9e8cca31d2020b33b7554f4c3dd48e981b60f
Gabvlz <gi.valenzuelaleon@gmail.com>
2023-08-02 15:47:55 -0400

    Common Prefix Python 3

--- Common Prefix/longestcommonprefix.py
+++ Common Prefix/longestcommonprefix.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        if not strs:
-            return "" #Condición solicitada en caso de no ser un prefijo o string válido
-        k = strs[0]
-        for i in range(1, len(k)): #rango inicia en la posición 1, no debemos comparar el primer string con sigo mismo
-            while strs[i].find(k) !=0: 
-                k = k[:-1] # Slice que regresa al carñacter anterior válido y cumplir con prefijo de busqueda
-                if not k:
-                    return ""
-        return k
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        best_prefix = strs[0]
+        for index,s in enumerate(strs):
+            print(index,s)
+            counter = 0
+            while counter < len(best_prefix) and counter < len(s) and best_prefix[counter] == s[counter]:
+                counter += 1
+            best_prefix = best_prefix[:counter]
+        return best_prefix
